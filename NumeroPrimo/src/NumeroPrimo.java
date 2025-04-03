92973a9fd7f87da264015181580b55a812a83db4
walterRafaGC <wrafagc@gmail.com>
2025-04-02 22:27:58 -0500

    Cambio de nombre a clase NumeroPrimo

--- NumeroPrimo/src/NumeroPrimo.java
+++ NumeroPrimo/src/NumeroPrimo.java
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import  java.util.Scanner;
-public class Main {
+public class NumeroPrimo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
